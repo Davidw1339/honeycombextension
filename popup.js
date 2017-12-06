ece514186eb2e0a81cb71133fc9febf8179cf07d
Cecilia Ding <csyding@gmail.com>
2017-12-06 16:11:22 -0600

    update to look less uggo and for some draggo

--- popup.js
+++ popup.js
@@ -4,36 +4,37 @@ $(function() {
 });
 
 $( document ).ready(function() {
-	if ($('input[type=checkbox]').prop('checked')) {
-		$( '#hitolo' ).click(toDisplayHTL);
-	}
+	$("#hitolo").click(toDisplayHTL);
+	$("#lotohi").click(toDisplayLTH);
+	$("#bese").click(toDisplayBS);
+	$("#tora").click(toDisplayTR);
+	$("#relevant").click(toDisplayREL);
+	$("#nw").click(toDisplayNEW);
 });
 
 function toDisplayHTL() {
     var x = document.getElementById("htl");
     var y = document.getElementById("lth");
-    if (x.style.display === "none") {
+    if ($('input[type=checkbox]').prop('checked') && x.style.display === "none") {
         x.style.display = "block";
         y.style.display = "none";
-    } else {
+    } else if (!$('input[type=checkbox]').prop('checked')) {
         x.style.display = "none";
-        y.style.display = "none";
     }
 }
 function toDisplayLTH() {
     var x = document.getElementById("lth");
     var y = document.getElementById('htl');
-    if (x.style.display === "none") {
+    if ($('input[type=checkbox]').prop('checked') && x.style.display === "none") {
         x.style.display = "block";
         y.style.display = "none";
-    } else {
+    } else if (!$('input[type=checkbox]').prop('checked')) {
         x.style.display = "none";
-        y.style.display = "none";
     }
 }
 function toDisplayBS() {
     var x = document.getElementById("bs");
-    if (x.style.display === "none") {
+    if ($('input[type=checkbox]').prop('checked') && x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
@@ -41,7 +42,7 @@ function toDisplayBS() {
 }
 function toDisplayTR() {
     var x = document.getElementById("tr");
-    if (x.style.display === "none") {
+    if ($('input[type=checkbox]').prop('checked') && x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
@@ -49,7 +50,7 @@ function toDisplayTR() {
 }
 function toDisplayREL() {
     var x = document.getElementById("relevant");
-    if (x.style.display === "none") {
+    if ($('input[type=checkbox]').prop('checked') && x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
@@ -57,7 +58,7 @@ function toDisplayREL() {
 }
 function toDisplayNEW() {
     var x = document.getElementById("new");
-    if (x.style.display === "none") {
+    if ($('input[type=checkbox]').prop('checked') && x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
