644ba12469789444f2e18686efe3acc002cd9606
ikukadia <kukadia2@illinois.edu>
2017-12-07 16:07:36 -0600

    uptodate

--- popup.js
+++ popup.js
@@ -1,15 +1,24 @@
 $(function() {
-    $( "#sortable" ).sortable(); ({
-        start: function(event, ui) {
-            var start_pos = ui.item.index();
-            ui.item.data('start_pos', start_pos);
-        },
-        update: function (event, ui) {
-            var start_pos = ui.item.data('start_pos');
-            var end_pos = ui.item.index();
-        }
-    });
-    $( "#sortable" ).disableSelection();
+    $( "#sortable" ).sortable(); // ({
+    //     start: function(event, ui) {
+    //         var start_pos = ui.item.index();
+    //         ui.item.data('start_pos', start_pos);
+    //     },
+    //     change: function(event, ui) {
+    //         var start_pos = ui.item.data('start_pos');
+    //         var index = ui.placeholder.index();
+    //         console.log(index);
+    //         if (start_pos < index) {
+    //             $('#sortable li:nth-child(' + index + ')').addClass('highlights');
+    //         } else {
+    //             $('#sortable li:eq(' + (index + 1) + ')').addClass('highlights');
+    //         }
+    //     },
+    //     update: function(event, ui) {
+    //         $('#sortable li').removeClass('highlights');
+    //     }
+    // });
+    // $( "#sortable" ).disableSelection();
 });
 
 $( document ).ready(function() {
