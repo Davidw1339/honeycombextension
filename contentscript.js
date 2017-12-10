a35d1b7202453e0a49074cdf4ab7c273f4f3fe50
ikukadia <kukadia2@illinois.edu>
2017-12-10 13:48:06 -0600

    debug

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,3 @@
-
-
 /**
 ->reorder the list with new rankings
 ->add parameters with weight on each option
@@ -8,144 +6,143 @@
 */
 
 function reorderList(woPrice, woNumRatings, woRating, highLowPrices) {
-     var items = $('.s-result-item.s-result-card-for-container.a-declarative.celwidget');
-     //rankings to sort array
-     var orderRankings = []
+    var items = $('.s-result-item.s-result-card-for-container.a-declarative.celwidget');
+    //rankings to sort array
+    var orderRankings = []
 
-     //ids for the array rankings
-     var idsReordered = []
+    //ids for the array rankings
+    var idsReordered = []
 
-     var highestNumReviews = getHighestNumReviews(items)
-     var highestPrice = getHighestPrice(items)
+    var highestNumReviews = getHighestNumReviews(items)
+    var highestPrice = getHighestPrice(items)
 
-     for (var i = 0; i < items.length; i++) {
-          //console.log(items[i])
+    for (var i = 0; i < items.length; i++) {
+        //console.log(items[i])
 
-          var item = $(items[i])
+        var item = $(items[i])
 
-          //<-----------get the title of the item----------->
-          var title = item.find(".a-link-normal.s-access-detail-page.s-color-twister-title-link.a-text-normal")
+        //<-----------get the title of the item----------->
+        var title = item.find(".a-link-normal.s-access-detail-page.s-color-twister-title-link.a-text-normal")
 
-          //<--------get the average of the whole prices--------->
-          var wholePriceArray = item.find('.sx-price-whole')
-          var sum = 0
-          for (var q = 0; q < wholePriceArray.length; q++) {
-               sum += parseInt(wholePriceArray[q].textContent)
-          }
+        //<--------get the average of the whole prices--------->
+        var wholePriceArray = item.find('.sx-price-whole')
+        var sum = 0
+        for (var q = 0; q < wholePriceArray.length; q++) {
+            sum += parseInt(wholePriceArray[q].textContent)
+        }
 
-          if (wholePriceArray.length != 0) {
-               sum /= wholePriceArray.length
+        if (wholePriceArray.length != 0) {
+            sum /= wholePriceArray.length
 
-               //wholeprice for the item
-               var wholePrice = (Math.round(sum))
-               sum = 0
+            //wholeprice for the item
+            var wholePrice = (Math.round(sum))
+            sum = 0
 
-               //<--------get the number of ratings per item--------->
-               var numberOfRatings = parseInt(item.find('.a-size-small.a-link-normal.a-text-normal').text())
+            //<--------get the number of ratings per item--------->
+            var numberOfRatings = parseInt(item.find('.a-size-small.a-link-normal.a-text-normal').text())
 
-               //<--------get the average rating per item--------->
-               var averageRatingBlock = item.find('.a-icon-alt')
-               var averageRating = -1
-               //if it doesn't have a rating, then set average and num ratings to 0
-               if (averageRatingBlock.length >= 1) {
-                    if (typeof averageRatingBlock[1] == 'undefined') {
-                         averageRating = 0
-                         numberOfRatings = 0
-                    } else if (averageRatingBlock[1].textContent.indexOf(".") != -1) {
-                         averageRating = averageRatingBlock[1].textContent.slice(0,3).valueOf()
-                    } else {
-                         averageRating = averageRatingBlock[1].textContent.slice(0,1).valueOf()
-                    }
-
-               }
-               if (typeof averageRatingBlock[1] == 'undefined') {
+            //<--------get the average rating per item--------->
+            var averageRatingBlock = item.find('.a-icon-alt')
+            var averageRating = -1
+            //if it doesn't have a rating, then set average and num ratings to 0
+            if (averageRatingBlock.length >= 1) {
+                if (typeof averageRatingBlock[1] == 'undefined') {
                     averageRating = 0
                     numberOfRatings = 0
-               }
+                } else if (averageRatingBlock[1].textContent.indexOf(".") != -1) {
+                    averageRating = averageRatingBlock[1].textContent.slice(0,3).valueOf()
+                } else {
+                    averageRating = averageRatingBlock[1].textContent.slice(0,1).valueOf()
+                }
+            }
+            
+            if (typeof averageRatingBlock[1] == 'undefined') {
+                averageRating = 0
+                numberOfRatings = 0
+            }
 
 
-               //create a ranking for the current item
-               var rankingRating = 0
+            //create a ranking for the current item
+            var rankingRating = 0
 
-               //each ranking is based of a linear scale, taking the highest of the page and using that
-               //as the 'base' number
+            //each ranking is based of a linear scale, taking the highest of the page and using that
+            //as the 'base' number
 
-               if (highLowPrices == "high") {
-                    rankingRating += (wholePrice/highestPrice) * 100 * woPrice
-               } else {
-                    rankingRating += ((1 - (wholePrice/highestPrice)) * 100 * woPrice)
-               }
-               rankingRating += (numberOfRatings/highestNumReviews) * 100 * woNumRatings
-               rankingRating += averageRating * 20 * woRating
+            if (highLowPrices == "high") {
+                rankingRating += (wholePrice/highestPrice) * 100 * woPrice
+            } else {
+                rankingRating += ((1 - (wholePrice/highestPrice)) * 100 * woPrice)
+            }
+            rankingRating += (numberOfRatings/highestNumReviews) * 100 * woNumRatings
+            rankingRating += averageRating * 20 * woRating
 
-               //adding elements to be sorted
-               orderRankings.push(rankingRating)
-               idsReordered.push(i)
-          }
-     }
+            //adding elements to be sorted
+            orderRankings.push(rankingRating)
+            idsReordered.push(i)
+        }
+    }
 
-     //return sorted elements
-     console.log(sortRankings(orderRankings,idsReordered))
-     return sortRankings(orderRankings,idsReordered)
+    //return sorted elements
+    console.log(sortRankings(orderRankings,idsReordered))
+    return sortRankings(orderRankings,idsReordered)
 }
 
 function getHighestPrice(items) {
 
-     //highest price of the item list
-     highest = 0
-     for(var i = 0; i < items.length; i++) {
-          var item = $(items[i])
-          var wholePriceArray = item.find('.sx-price-whole')
-          var sum = 0
-          for (var q = 0; q < wholePriceArray.length; q++) {
-               sum += parseInt(wholePriceArray[q].textContent)
-          }
-          sum /= wholePriceArray.length
-          var wholePrice = (Math.round(sum))
-          if (wholePrice > highest) {
-               highest = wholePrice
-          }
-          sum = 0
-     }
-     return highest
+    //highest price of the item list
+    highest = 0
+    for(var i = 0; i < items.length; i++) {
+        var item = $(items[i])
+        var wholePriceArray = item.find('.sx-price-whole')
+        var sum = 0
+        for (var q = 0; q < wholePriceArray.length; q++) {
+            sum += parseInt(wholePriceArray[q].textContent)
+        }
+        sum /= wholePriceArray.length
+        var wholePrice = (Math.round(sum))
+        if (wholePrice > highest) {
+            highest = wholePrice
+        }
+        sum = 0
+    }
+    return highest
 }
 
 function getHighestNumReviews(items) {
 
-     //highest numReviews of the item list
-     highest = 0
-     for(var i = 0; i < items.length; i++) {
-          var item = $(items[i])
-          var numberOfRatings = parseInt(item.find('.a-size-small.a-link-normal.a-text-normal').text())
-          if (highest < numberOfRatings) {
-               highest = numberOfRatings
-          }
-     }
-     return highest
+    //highest numReviews of the item list
+    highest = 0
+    for(var i = 0; i < items.length; i++) {
+        var item = $(items[i])
+        var numberOfRatings = parseInt(item.find('.a-size-small.a-link-normal.a-text-normal').text())
+        if (highest < numberOfRatings) {
+            highest = numberOfRatings
+        }
+    }
+    return highest
 }
 
 function sortRankings(rankings, resultIds) {
-     //using a bubble sort
-     for (var i = 0; i < rankings.length; i++) {
-          for (var q = 1; q < rankings.length - i; q++) {
-               if (rankings[q-1] < rankings[q]) {
+    //using a bubble sort
+    for (var i = 0; i < rankings.length; i++) {
+        for (var q = 1; q < rankings.length - i; q++) {
+            if (rankings[q-1] < rankings[q]) {
 
-                    //sort rankings
-                    temp = rankings[q-1]
-                    rankings[q-1] = rankings[q]
-                    rankings[q] = temp
+                //sort rankings
+                temp = rankings[q-1]
+                rankings[q-1] = rankings[q]
+                rankings[q] = temp
 
-                    //based off of rankings, reorder the result_ids
-                    temp2 = resultIds[q-1]
-                    resultIds[q-1] = resultIds[q]
-                    resultIds[q] = temp2
-               }
-          }
-     }
+                //based off of rankings, reorder the result_ids
+                temp2 = resultIds[q-1]
+                resultIds[q-1] = resultIds[q]
+                resultIds[q] = temp2
+            }
+        }
+    }
 
-     console.log(resultIds)
-     return resultIds
-
+    console.log(resultIds)
+    return resultIds
 }
 
 function reorderHTML(parameter) {
@@ -159,6 +156,5 @@ function reorderHTML(parameter) {
         }
     }
 }
-
 
 $(document).ready(reorderHTML(reorderList(0.33,0.33,0.33,"low")));
